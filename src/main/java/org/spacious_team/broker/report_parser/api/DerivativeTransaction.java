bfaa19033975660a7f43071312fd1b4a6f8b3f7a
Vitalii Ananev <vananiev@users.noreply.github.com>
2021-02-17 23:52:34 +0300

    permit zero values for DerivativeTransaction

--- src/main/java/org/spacious_team/broker/report_parser/api/DerivativeTransaction.java
+++ src/main/java/org/spacious_team/broker/report_parser/api/DerivativeTransaction.java
@@ -47,13 +47,27 @@ public class DerivativeTransaction extends AbstractTransaction {
     }
 
     protected Optional<TransactionCashFlow> getValueInPointsCashFlow() {
-        if (valueInPoints != null && valueInPoints.abs().compareTo(minValue) >= 0) {
+        if (valueInPoints != null) {
             return Optional.of(TransactionCashFlow.builder()
                     .transactionId(transactionId)
                     .portfolio(portfolio)
                     .eventType(CashFlowType.DERIVATIVE_QUOTE)
                     .value(valueInPoints)
                     .currency(QUOTE_CURRENCY)
+                    .build());
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    protected Optional<TransactionCashFlow> getValueCashFlow(CashFlowType type) {
+        if (value != null) {
+            return Optional.of(TransactionCashFlow.builder()
+                    .transactionId(transactionId)
+                    .portfolio(portfolio)
+                    .eventType(type)
+                    .value(value)
+                    .currency(valueCurrency)
                     .build());
         }
         return Optional.empty();
