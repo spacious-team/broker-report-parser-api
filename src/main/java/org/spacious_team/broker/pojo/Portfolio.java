926726b5d594763da03f03d42158e97ca80780e0
AleksandrKlimchuk <1Vipe234@gmail.com>
2021-08-06 20:32:52 +0300

    Add field enabled to Portfolio

--- src/main/java/org/spacious_team/broker/pojo/Portfolio.java
+++ src/main/java/org/spacious_team/broker/pojo/Portfolio.java
@@ -39,4 +39,9 @@ public class Portfolio {
     @NotNull
     @Schema(description = "Номер счета в системе учета брокера", example = "10200I", required = true)
     private final String id;
+
+    @NotNull
+    @Schema(description = "Признак активного счета: если false то счет исключается из аналитического расчета",
+            required = true)
+    private final boolean enabled;
 }
